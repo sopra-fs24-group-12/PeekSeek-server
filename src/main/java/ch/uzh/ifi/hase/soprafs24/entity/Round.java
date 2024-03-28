6e165c7f1a2f0ec999f255d6effadcad83e8e6a3
Georg Emmermann <georg.emmermann@uzh.ch>
2024-03-28 17:22:50 +0100

    fixed missing getters and setters

--- src/main/java/ch/uzh/ifi/hase/soprafs24/entity/Round.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/entity/Round.java
@@ -22,4 +22,59 @@ public class Round {
     @JoinColumn(name = "game_id")
     private Long game;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getQuest() {
+        return quest;
+    }
+
+    public void setQuest(String quest) {
+        this.quest = quest;
+    }
+
+    public int getRoundTime() {
+        return roundTime;
+    }
+
+    public void setRoundTime(int roundTime) {
+        this.roundTime = roundTime;
+    }
+
+    public RoundStatus getRoundStatus() {
+        return roundStatus;
+    }
+
+    public void setRoundStatus(RoundStatus roundStatus) {
+        this.roundStatus = roundStatus;
+    }
+
+    public List<Submission> getSubmissions() {
+        return submissions;
+    }
+
+    public void setSubmissions(List<Submission> submissions) {
+        this.submissions = submissions;
+    }
+
+    public Submission getWinningSubmission() {
+        return winningSubmission;
+    }
+
+    public void setWinningSubmission(Submission winningSubmission) {
+        this.winningSubmission = winningSubmission;
+    }
+
+    public Long getGame() {
+        return game;
+    }
+
+    public void setGame(Long game) {
+        this.game = game;
+    }
 }
