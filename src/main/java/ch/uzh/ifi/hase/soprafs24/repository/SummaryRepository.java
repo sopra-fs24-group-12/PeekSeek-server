8e2b6097ab2259d57cff6b065a5c6cc7e33e4a31
Georg Emmermann <georg.emmermann@uzh.ch>
2024-04-13 19:29:01 +0200

    added findById method in summary repository

--- src/main/java/ch/uzh/ifi/hase/soprafs24/repository/SummaryRepository.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/repository/SummaryRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository("summaryRepository")
 public interface SummaryRepository extends JpaRepository<Summary, Long> {
+    public Summary findSummaryById(long id);
 }
