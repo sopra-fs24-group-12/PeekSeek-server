bc88c7f6485868906b0756384f776cb155f3dcaa
Figo2003 <youssef.ehabyousseffarag@uzh.ch>
2024-04-13 17:24:24 +0200

    close #117

--- src/test/java/ch/uzh/ifi/hase/soprafs24/rest/mapper/DTOMapperTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs24/rest/mapper/DTOMapperTest.java
@@ -1,9 +1,11 @@
 package ch.uzh.ifi.hase.soprafs24.rest.mapper;
 
-import ch.uzh.ifi.hase.soprafs24.constant.UserStatus;
-import ch.uzh.ifi.hase.soprafs24.entity.User;
-import ch.uzh.ifi.hase.soprafs24.rest.dto.UserGetDTO;
-import ch.uzh.ifi.hase.soprafs24.rest.dto.UserPostDTO;
+
+import ch.uzh.ifi.hase.soprafs24.entity.Lobby;
+import ch.uzh.ifi.hase.soprafs24.entity.Participant;
+import ch.uzh.ifi.hase.soprafs24.rest.dto.LobbyGetDTO;
+import ch.uzh.ifi.hase.soprafs24.rest.dto.LobbyPostDTO;
+import ch.uzh.ifi.hase.soprafs24.rest.dto.ParticipantGetDTO;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -14,37 +16,42 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  * works.
  */
 public class DTOMapperTest {
-  @Test
-  public void testCreateUser_fromUserPostDTO_toUser_success() {
-    // create UserPostDTO
-    UserPostDTO userPostDTO = new UserPostDTO();
-    userPostDTO.setName("name");
-    userPostDTO.setUsername("username");
 
-    // MAP -> Create user
-    User user = DTOMapper.INSTANCE.convertUserPostDTOtoEntity(userPostDTO);
+    @Test
+    public void test_convert_Lobby_To_LobbyGetDTO() {
+        Lobby lobby = new Lobby();
+        lobby.setId(3L);
+        lobby.setMaxParticipants(5);
+        lobby.setJoinedParticipants(3);
+        lobby.setName("Beispiel");
 
-    // check content
-    assertEquals(userPostDTO.getName(), user.getName());
-    assertEquals(userPostDTO.getUsername(), user.getUsername());
-  }
+        LobbyGetDTO lobbyGetDTO = DTOMapper.INSTANCE.convertLobbyToLobbyGetDTO(lobby);
+        assertEquals(lobby.getId(), lobbyGetDTO.getId());
+        assertEquals(lobby.getName(), lobbyGetDTO.getName());
+        assertEquals(lobby.getJoinedParticipants(), lobbyGetDTO.getJoinedParticipants());
+        assertEquals(lobby.getMaxParticipants(), lobbyGetDTO.getMaxParticipants());
 
-  @Test
-  public void testGetUser_fromUser_toUserGetDTO_success() {
-    // create User
-    User user = new User();
-    user.setName("Firstname Lastname");
-    user.setUsername("firstname@lastname");
-    user.setStatus(UserStatus.OFFLINE);
-    user.setToken("1");
 
-    // MAP -> Create UserGetDTO
-    UserGetDTO userGetDTO = DTOMapper.INSTANCE.convertEntityToUserGetDTO(user);
+    }
 
-    // check content
-    assertEquals(user.getId(), userGetDTO.getId());
-    assertEquals(user.getName(), userGetDTO.getName());
-    assertEquals(user.getUsername(), userGetDTO.getUsername());
-    assertEquals(user.getStatus(), userGetDTO.getStatus());
-  }
+    @Test
+    public void test_convert_Participant_To_ParticipantGetDTO() {
+        Participant participant = new Participant();
+        participant.setId(3L);
+        participant.setAdmin(Boolean.TRUE);
+        participant.setUsername("Beispiel");
+        participant.setScore(33);
+        participant.setStreak(2);
+        participant.setLeftGame(Boolean.TRUE);
+
+        ParticipantGetDTO participantGetDTO = DTOMapper.INSTANCE.convertParticipantToParticipantGetDTO(participant);
+        assertEquals(participant.getId(), participantGetDTO.getId());
+        assertEquals(participant.getAdmin(), participantGetDTO.getAdmin());
+        assertEquals(participant.getUsername(), participantGetDTO.getUsername());
+        assertEquals(participant.getScore(), participantGetDTO.getScore());
+        assertEquals(participant.getStreak(), participantGetDTO.getStreak());
+        assertEquals(participant.getLeftGame(), participantGetDTO.getLeftGame());
+
+    }
 }
+
