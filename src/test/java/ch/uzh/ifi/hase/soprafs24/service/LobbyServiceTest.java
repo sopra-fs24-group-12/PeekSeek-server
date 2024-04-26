dcbb22f4385706d9fd47308e9011e43c2567c5e2
Georg Emmermann <georg.emmermann@uzh.ch>
2024-04-26 09:17:09 +0200

    test added

--- src/test/java/ch/uzh/ifi/hase/soprafs24/service/LobbyServiceTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs24/service/LobbyServiceTest.java
@@ -1,6 +1,7 @@
 package ch.uzh.ifi.hase.soprafs24.service;
 
 import ch.uzh.ifi.hase.soprafs24.entity.Lobby;
+import ch.uzh.ifi.hase.soprafs24.entity.Participant;
 import ch.uzh.ifi.hase.soprafs24.repository.GeoCodingDataRepository;
 import ch.uzh.ifi.hase.soprafs24.repository.LobbyRepository;
 import ch.uzh.ifi.hase.soprafs24.service.LobbyService;
@@ -16,6 +17,7 @@ import org.springframework.web.server.ResponseStatusException;
 import javax.persistence.Lob;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -46,6 +48,7 @@ public class LobbyServiceTest {
         lobby.setName("lobby");
         lobby.setPassword("test");
         lobby.setPasswordProtected(true);
+        LobbyRepository.addLobby(lobby);
     }
 
     @Test
@@ -61,6 +64,37 @@ public class LobbyServiceTest {
         assertTrue(createdLobby.getPasswordProtected());
         verify(lobbyRepositoryInstance, times(1));
         LobbyRepository.addLobby(createdLobby);
+    }
+
+    @Test
+    public void testJoinLobby() {
+        String username = "test";
+        String password = "test";
+        String token = lobbyService.joinLobby(1L, username, password);
+
+        Map<String, Participant> joinedParticipants = LobbyRepository.getLobbyById(1L).getParticipants();
+
+        assert(joinedParticipants.values().size() == 1);
+        assert(joinedParticipants.get(token).getUsername().equals(username));
+        assert(joinedParticipants.get(token).getId() == 1L);
+        assert(joinedParticipants.get(token).getAdmin());
+        assert(lobbyService.getSpecificLobby(1L).getJoinedParticipants() == 1);
+        assert(lobbyService.getSpecificLobby(1L).getAdminId() == 1L);
+        assert(lobbyService.getSpecificLobby(1L).getUsernames().contains("test"));
+    }
+
+    @Test
+    public void testLeaveLobby() {
+        String username = "test";
+        String password = "test";
+        String token = lobbyService.joinLobby(1L, username, password);
+
+        Map<String, Participant> joinedParticipants = LobbyRepository.getLobbyById(1L).getParticipants();
+
+        lobbyService.leaveLobby(1L, token);
+        assert(joinedParticipants.values().isEmpty());
+        assert(lobbyService.getSpecificLobby(1L).getJoinedParticipants() == 0);
+        assert(!lobbyService.getSpecificLobby(1L).getUsernames().contains("test"));
     }
 
 }
