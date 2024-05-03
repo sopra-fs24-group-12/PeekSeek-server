b7b9a4aedb1ec3c7d65a918d59efe2869f59484f
Figo2003 <youssef.ehabyousseffarag@uzh.ch>
2024-05-03 13:29:45 +0300

    #181
    SubmissionDataTest
    SubmissionTest
    SummaryTest

--- src/test/java/ch/uzh/ifi/hase/soprafs24/entity/SubmissionDataTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs24/entity/SubmissionDataTest.java
@@ -1,5 +1,57 @@
 package ch.uzh.ifi.hase.soprafs24.entity;
 
+
+import ch.uzh.ifi.hase.soprafs24.entity.SubmissionData;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
 public class SubmissionDataTest {
-  
+
+    private SubmissionData submissionData;
+
+    @BeforeEach
+    public void setUp() {
+        submissionData = new SubmissionData();
+    }
+    @AfterEach
+    public void tearDown() { submissionData = null; }
+
+    @Test
+    public void testSetAndGetLat() {
+        String lat = "47.1234";
+        submissionData.setLat(lat);
+        assertEquals(lat, submissionData.getLat());
+    }
+
+    @Test
+    public void testSetAndGetLng() {
+        String lng = "8.5678";
+        submissionData.setLng(lng);
+        assertEquals(lng, submissionData.getLng());
+    }
+
+    @Test
+    public void testSetAndGetHeading() {
+        String heading = "90";
+        submissionData.setHeading(heading);
+        assertEquals(heading, submissionData.getHeading());
+    }
+
+    @Test
+    public void testSetAndGetPitch() {
+        String pitch = "45";
+        submissionData.setPitch(pitch);
+        assertEquals(pitch, submissionData.getPitch());
+    }
+
+    @Test
+    public void testSetAndGetNoSubmission() {
+        Boolean noSubmission = true;
+        submissionData.setNoSubmission(noSubmission);
+        assertEquals(noSubmission, submissionData.getNoSubmission());
+    }
 }
--- src/test/java/ch/uzh/ifi/hase/soprafs24/entity/SummaryTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs24/entity/SummaryTest.java
@@ -1,5 +1,57 @@
 package ch.uzh.ifi.hase.soprafs24.entity;
 
+import ch.uzh.ifi.hase.soprafs24.entity.summary.Summary;
+import ch.uzh.ifi.hase.soprafs24.entity.summary.Quest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class SummaryTest {
-  
+
+    private Summary summary;
+
+    @BeforeEach
+    public void setUp() {
+        summary = new Summary();
+    }
+
+    @Test
+    public void testSetAndGetId() {
+        Long id = 123L;
+        summary.setId(id);
+        assertEquals(id, summary.getId());
+    }
+
+    @Test
+    public void testSetAndGetCityName() {
+        String cityName = "Zurich";
+        summary.setCityName(cityName);
+        assertEquals(cityName, summary.getCityName());
+    }
+
+    @Test
+    public void testSetAndGetRoundsPlayed() {
+        int roundsPlayed = 5;
+        summary.setRoundsPlayed(roundsPlayed);
+        assertEquals(roundsPlayed, summary.getRoundsPlayed());
+    }
+
+    @Test
+    public void testSetAndGetQuests() {
+        List<Quest> quests = new ArrayList<>();
+        quests.add(new Quest());
+        summary.setQuests(quests);
+        assertEquals(quests, summary.getQuests());
+    }
+
+    @Test
+    public void testSetAndGetPassword() {
+        String password = "password123";
+        summary.setPassword(password);
+        assertEquals(password, summary.getPassword());
+    }
 }
