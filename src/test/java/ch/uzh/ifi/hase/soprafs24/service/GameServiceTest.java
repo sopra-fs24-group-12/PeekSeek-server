b1770c5fdce737b3c84d8eef7e8dc5cd39712983
Figo2003 <youssef.ehabyousseffarag@uzh.ch>
2024-05-08 11:45:43 +0200

    #113

--- src/test/java/ch/uzh/ifi/hase/soprafs24/service/GameServiceTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs24/service/GameServiceTest.java
@@ -116,9 +116,7 @@ public class GameServiceTest {
         //when(mock.gameRepository.getGameById(anyLong())).thenReturn(game);
         //given(gameRepository.getGameById(game.getId())).willReturn(game);
         //when(gameService.getSpecificGame(1L)).thenReturn(game);
-        System.out.println("This is the admin- participant");
-        System.out.println(game.getId());
-        System.out.println(game.getParticipantByToken("abc"));
+
 
         List<Participant> par = gameService.getLeaderboard("abc", 1L);
 
@@ -127,317 +125,7 @@ public class GameServiceTest {
         //assert(par.get(0).getUsername().equals(participant.getUsername()));
         //assert("m".equals("m"));
     }
- /*
-    @Test
-    public void testStartGame() throws IOException {
-        Participant participant = new Participant();
-        participant.setId(1L);
-        participant.setUsername("test");
-        participant.setAdmin(true);
-        participant.setToken("abc");
 
-        Participant participant2 = new Participant();
-        participant.setId(2L);
-        participant.setUsername("test2");
-        participant.setAdmin(false);
-        participant.setToken("abc2");
-
-        Participant participant3 = new Participant();
-        participant.setId(3L);
-        participant.setUsername("test3");
-        participant.setAdmin(false);
-        participant.setToken("abc3");
-
-        lobby.setAdminId(1L);
-        lobby.setAdminUsername("test");
-        lobby.addParticipant(participant);
-        lobby.addParticipant(participant2);
-        lobby.addParticipant(participant3);
-
-        lobby.setJoinedParticipants(3);
-        lobby.setRoundDurationSeconds(60);
-        lobby.setGameLocation("Zurich");
-        List<String> quests = new ArrayList<>();
-        quests.add("item 1");
-        quests.add("item 2");
-
-        Game createdGame = new Game();
-
-        List<Round> rounds = new ArrayList<>(quests.size());
-        for (String quest : quests) {
-            Round round = new Round();
-            round.setId(Game.rounds_count++);
-            round.setQuest(quest);
-            round.setRoundTime(lobby.getRoundDurationSeconds());
-            round.setRemainingSeconds(lobby.getRoundDurationSeconds());
-            round.setGeoCodingData(lobby.getGameLocationCoordinates());
-            rounds.add(round);
-        }
-        createdGame.setRounds(rounds);
-        createdGame.setRoundDurationSeconds(lobby.getRoundDurationSeconds());
-        createdGame.setAdminId(lobby.getAdminId());
-        createdGame.setGameLocation(lobby.getGameLocation());
-        createdGame.setNumberRounds(rounds.size());
-        createdGame.setLobbyPassword(lobby.getPassword());
-
-        Map<String, Participant> participants = new HashMap<>(lobby.getParticipants());
-        createdGame.setParticipants(participants);
-        createdGame.setActiveParticipants(createdGame.getParticipants().size());
-
-        gameRepository.addGame(createdGame);
-        when(gameService.getSpecificGame(1L)).thenReturn(game);
-        Long l = gameService.startGame(lobby);
-
-        assert(createdGame.getId().equals(l));
-        //assert(createdGame.getGameLocation().equals(gameRepository.findById(l).getGameLocation()));
-
-    }
-
-    @Test
-    public void testHandleMissingSubmission() {
-        Participant participant = new Participant();
-        participant.setId(7L);
-        participant.setUsername("test");
-        participant.setAdmin(true);
-        participant.setToken("abc");
-        participant.setScore(200);
-
-        Participant participant2 = new Participant();
-        participant2.setId(4L);
-        participant2.setUsername("test2");
-        participant2.setAdmin(false);
-        participant2.setToken("abc2");
-        participant2.setScore(203);
-
-        Participant participant3 = new Participant();
-        participant3.setId(5L);
-        participant3.setUsername("test3");
-        participant3.setAdmin(false);
-        participant3.setToken("abc3");
-        participant3.setScore(20);
-
-        game.setAdminId(7L);
-        Map<String, Participant> participants1 = new HashMap<>();
-        participants1.put("test", participant);
-        participants1.put("test2", participant2);
-        participants1.put("test3", participant3);
-        game.setParticipants(participants1);
-        game.setActiveParticipants(3);
-
-        Map<String, Participant> participants = game.getParticipants();
-
-        Round round = new Round();
-        round.setRoundTime(60);
-
-        for (Participant par : participants.values()) {
-            if (!par.getHasSubmitted() && !par.getLeftGame()) {
-                SubmissionData submissionData = new SubmissionData();
-                submissionData.setLat("47.3768866");
-                submissionData.setLng("8.541694");
-                submissionData.setPitch("50");
-                submissionData.setHeading("50");
-
-                Submission emptySubmission = new Submission();
-
-                emptySubmission.setId(Round.submissionCount++);
-                emptySubmission.setSubmissionTimeSeconds(round.getRoundTime());
-                emptySubmission.setToken(par.getToken());
-                emptySubmission.setUsername(par.getUsername());
-                emptySubmission.setNoSubmission(true);
-                emptySubmission.setSubmittedLocation(submissionData);
-                round.addSubmission(emptySubmission);
-            }
-            assertFalse(par.getHasSubmitted());
-            assert(round.getSubmissions().equals())
-
-        }
-        Submission sub = new Submission();
-        sub.setId(Round.submissionCount++);
-        sub.setSubmissionTimeSeconds(round.getRoundTime());
-        sub.setToken(par.getToken());
-        sub.setUsername(par.getUsername());
-        sub.setNoSubmission(true);
-        sub.setSubmittedLocation(submissionData);
-        round.addSubmission(emptySubmission);
-        Map<Long, Submission> submissions = new HashMap<>();
-        submissions.put();
-        assert (round.getRoundTime() == 60);
-        assert(round.getSubmissions().equals(participants));
-    }*/
-    /*
-    @Test
-    public void testHandleMissingSubmissions() {
-
-    Round round = new Round();
-    round.setRoundTime(123);
-    Long gameId = 1L;
-
-    Map<String, Participant> participants = new HashMap<>();
-    Participant participantWithMissingSubmission = new Participant();
-    participantWithMissingSubmission.setToken("participantToken");
-    participantWithMissingSubmission.setUsername("participantUsername");
-    participantWithMissingSubmission.setHasSubmitted(false);
-    participantWithMissingSubmission.setLeftGame(false);
-    participants.put("participantToken", participantWithMissingSubmission);
-    game.setParticipants(participants);
-    when(gameService.getSpecificGame(gameId)).thenReturn(game);
-
-    gameService.handleMissingSubmissions(round, gameId);
-
-
-    assertEquals(1, round.getSubmissions().size());
-    Submission submission = round.getSubmissions().get(0);
-    assertEquals("participantToken", submission.getToken());
-    assertEquals("participantUsername", submission.getUsername());
-    //assertTrue(submission.isNoSubmission());
-    assertEquals(123, submission.getSubmissionTimeSeconds());
-}
-
-    @Test
-    public void testNoSubmission() {
-        Long gameId = 1L;
-        Round round = new Round();
-        Submission submission = new Submission();
-        submission.setNoSubmission(true);
-        when(gameRepository.getGameById(gameId)).thenReturn(game);
-        int points = gameService.calculatePoints(gameId, round, submission, 1);
-        System.out.println(points);
-        assertEquals(0, points);
-    }
-
-    @Test
-    public void testHalfBanVotes() {
-        Long gameId = 1L;
-        Round round = new Round();
-        round.getSubmissions().put(1L, new Submission());
-        round.getSubmissions().put(2L, new Submission());
-        Submission submission = new Submission();
-        submission.setNumberBanVotes(1);
-
-        Participant participant = new Participant();
-        game.getParticipants().put(submission.getToken(), participant);
-
-
-        when(gameRepository.getGameById(gameId)).thenReturn(game);
-        int points = gameService.calculatePoints(gameId, round, submission, 1);
-
-        assertEquals(0, points);
-    }
-
-
-
-
-    @Test
-    public void testFullPointsNoBonus() {
-        Long gameId = 1L;
-        Round round = new Round();
-        round.setRoundTime(60);
-        round.getSubmissions().put(1L, new Submission());
-        Submission submission = new Submission();
-        submission.setSubmissionTimeSeconds(0);
-        submission.setNumberVotes(2);
-
-        Game game = mock(Game.class);
-        Map<String, Participant> participants = new HashMap<>();
-        Participant participant = new Participant();
-        participant.setToken("participantToken");
-        participants.put(participant.getToken(), participant);
-        game.setParticipants(participants);
-        GameRepository.getGameById = game -> game;
-
-        int points = gameService.calculatePoints(gameId, round, submission, 1);
-
-        assertEquals(775, points);  // 250 (time) + 250 (placement) + 275 (voting)
-    }
-
-    @Test
-    public void testFullPointsWithWinningSubmission() {
-        Long gameId = 1L;
-        Round round = new Round();
-        round.setRoundTime(60);
-        round.getSubmissions().put(1L, new Submission());
-        Submission submission = new Submission();
-        submission.setSubmissionTimeSeconds(0);
-        submission.setNumberVotes(2);  // assume 2 other participants voted
-
-        Game game = mock(Game.class);
-        Map<String, Participant> participants = new HashMap<>();
-        Participant participant = new Participant();
-        participant.setToken("participantToken");
-        participants.put(participant.getToken(), participant);
-        game.setParticipants(participants);
-        GameRepository.getGameById = game -> game;  // mock GameRepository
-
-        int points = gameService.calculatePoints(gameId, round, submission, 1);
-        round.setWinningSubmission(submission);
-
-        assertEquals(1162, points);  // 775 (base points) * 1.5 (winning bonus)
-        assertEquals(1, participant.getWinningSubmissions());
-        assertEquals(1, participant.getStreak());
-    }
-
-    @Test
-    public void testPointsWithStreakBonus() {
-        Long gameId = 1L;
-        Round round = new Round();
-        round.setRoundTime(60);
-        round.getSubmissions().put(1L, new Submission());
-        Submission submission = new Submission();
-        submission.setSubmissionTimeSeconds(0);
-        submission.setNumberVotes(2);  // assume 2 other participants voted
-
-        Game game = mock(Game.class);
-        Map<String, Participant> participants = new HashMap<>();
-        Participant participant = new Participant();
-        participant.setStreak(2);
-        participant.setToken("participantToken");
-        participants.put(participant.getToken(), participant);
-        game.setParticipants(participants);
-        GameRepository.getGameById = game -> game;  // mock GameRepository
-
-        int points = calculatePoints(gameId, round, submission, 1);
-
-        assertTrue(points > 775);  // points should be higher due to streak bonus
-        assertEquals(participant.getStreak(), 0);  // streak should reset if not winning submission
-    }*/
-    /*
-@Test
-public void testAwardPoints() {
-    // Mock data
-    Long gameId = 1L;
-    Round round = new Round();
-    Submission submission1 = new Submission();
-    Submission submission2 = new Submission();
-    submission1.setToken("token1");
-    submission2.setToken("token2");
-    round.getSubmissions().put(1L, submission1);
-    round.getSubmissions().put(2L, submission2);
-
-    // Mock game and participant
-    Game game = new Game();
-    game.setId(gameId);
-    Participant participant1 = new Participant();
-    Participant participant2 = new Participant();
-    game.getParticipants().put(submission1.getToken(), participant1);
-    game.getParticipants().put(submission2.getToken(), participant2);
-
-    // Set up mock behavior
-    when(gameRepository.getGameById(gameId)).thenReturn(game);
-
-    // Mock the calculatePoints method
-    PointsCalculator calculator = spy(new PointsCalculator(gameRepository));
-    doReturn(500).when(calculator).calculatePoints(eq(gameId), any(Round.class), eq(submission1), eq(1));
-    doReturn(750).when(calculator).calculatePoints(eq(gameId), any(Round.class), eq(submission2), eq(2));
-
-    // Call the method
-    calculator.awardPoints(round, gameId);
-
-    // Assertions
-    assertEquals(500, submission1.getAwardedPoints());
-    assertEquals(750, submission2.getAwardedPoints());
-}
-
-     */
     @Test
     public void TestSetWinningSubmission(){
         Round round = new Round();
@@ -461,8 +149,6 @@ public void testAwardPoints() {
         submissions.sort(Comparator.comparing(Submission::getNumberVotes).reversed().thenComparing(Submission::getSubmissionTimeSeconds));
         round2.setWinningSubmission(submissions.get(0));
 
-        //System.out.println(round.getWinningSubmission().getSubmissionTimeSeconds());
-        //System.out.println(round2.getWinningSubmission().getSubmissionTimeSeconds());
 
 
         assert(round2.getWinningSubmission().getSubmissionTimeSeconds().equals(round.getWinningSubmission().getSubmissionTimeSeconds()));
@@ -532,13 +218,10 @@ public void testAwardPoints() {
                 round211.addSubmission(emptySubmission);
             }
         }
-        //given(gameService.getSpecificGame(1L)).willReturn(game);
+
         gameService.handleMissingSubmissions(round111, gameId);
         List<Submission> r1 = new ArrayList<>(round111.getSubmissions().values());
         List<Submission> r2 = new ArrayList<>(round211.getSubmissions().values());
-
-        //System.out.println(r1.get(0).getToken());
-        //System.out.println(r2.get(0).getToken());
 
 
 
@@ -618,15 +301,14 @@ public void testAwardPoints() {
                 }
             }, 3000);
         }
-        //given(gameService.getSpecificGame(1L)).willReturn(game);
+
         Round currentRound = game.getRounds().get(game.getCurrentRound());
         System.out.println(cRound.getSubmissions().values());
         participant.setHasSubmitted(Boolean.FALSE);
         gameService.postSubmission(1L, "abc", submissionPostDTO);
 
 
-        //System.out.println(cRound.getSubmissions().values());
-        //System.out.println(currentRound.getSubmissions().values());
+
 
 
 
